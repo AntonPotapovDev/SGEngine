53f2f2bbd19b1933b3c2d76182f7e2e7cb704b6c
Anton Potapov <protestandprotect52@gmail.com>
2018-12-05 11:52:15 +0300

    Added class SGModel, fixed constructor of SGTexture

---
+++ model.h
@@ -0,0 +1,81 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include "context/glew.h"
+#include "texture.h"
+
+/*
+    This class represents a 3D model. It stores 3D coordinates and
+	is used by the constructor of the class 'WorldObject'
+*/
+class SGModel
+{
+public:
+
+	// Constructor: generates buffer-objects 
+	// and sets all pointers to NULL
+	SGModel();
+
+	// Constructor: fills the vertex coordinates array 
+	// using data from specified file and sets new texture of the model
+	SGModel
+	(
+		const char* path,      // Path to file
+		SGTexture newTexture
+	);
+
+	// Constructor: fills the vertex coordinates array 
+	// using data from specified array and sets new texture of the model
+	SGModel
+	(
+		GLfloat* newCoords,    // Pointer to vertex coordinates array
+		int length,            // Length of vertex coordinates array
+		SGTexture newTexture
+	);
+
+	// Destructor: frees up model resources
+	~SGModel();
+
+	// Setter: sets the order in which openGL will draw vertices
+	// (loads array of indices to IBO)
+	// (it necessary to be set only if you want to use 'drawWithIBO()')
+	void setOrderOfVertices
+	(
+		int* order   // Order of vertices
+	);
+
+	// Getter: returns the pointer to the vertex array of the model
+	GLfloat getCoords();
+
+	// Getter: returns count of vertices of the model
+	int getCountOfVertices();
+
+	// Getter: returns the texture of the model
+	SGTexture getTexture();
+
+	// Getter: returns ID of vertex buffer object
+	GLuint getVBO();
+
+	// Getter: returns ID of vertex array object
+	GLuint getVAO();
+
+	// Getter: returns ID of index buffer object
+	GLuint getIBO();
+
+	// Draws model using default order of vertices  
+	void draw();
+
+	// Draws model using the order which is setted in IBO
+	void drawWithIBO();
+
+private:
+	GLfloat* coords;      // 3D-Coordinates of the model
+	int countOfVertices;  // Count of 3D-Coordinates of the model
+	SGTexture texture;  // Texture of the model
+	GLuint VBO;         // Vertex buffer object
+   	GLuint VAO;         // Vertex array object
+	GLuint IBO;         // Index buffer object
+};
+
+#endif 
+
--- texture.cpp
+++ texture.cpp
@@ -25,9 +25,19 @@ SGTexture::SGTexture(const char* path, WrappingType sType, WrappingType tType, F
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minFilter);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, magFilter);
 
-	image = SOIL_load_image(path, &width, &height, 0, SOIL_LOAD_RGB);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-	glGenerateMipmap(GL_TEXTURE_2D);
+	try
+	{
+		image = NULL;
+		image = SOIL_load_image(path, &width, &height, 0, SOIL_LOAD_RGB);
+		if (image == NULL)
+			throw std::exception("Error: texture initialization failed!");
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	catch (std::exception ex)
+	{
+		throw std::exception("Error: texture loading failed!");
+	}
 
 	SOIL_free_image_data(image);
 
