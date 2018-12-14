aaff99c56d80031feaec3a22d7af957c069308b7
Anton Potapov <protestandprotect52@gmail.com>
2018-12-14 23:16:18 +0300

    Added documentation to SGWorld

--- SGEngine/world.h
+++ SGEngine/world.h
@@ -5,26 +5,33 @@
 #include "camera.h"
 #include "worldobject.h"
 
+/*
+    This class represents a 3D graphical world which can be drawn by openGL.
+	Object of this type stores pointers to window object (GWindow) and camera object (SGCamera)
+*/
 class SGWorld
 {
 public:
+	// Constructor: creates a world object and sets all pointers to NULL
 	SGWorld();
 
+	// Constructor: creates a world object and sets its window and camera 
 	SGWorld
 	(
 		GWindow* window, 
 		SGCamera* camera
 	);
 
+	// Draw specified array of SGWorldObject's 
 	void draw
 	(
-		SGWorldObject* toDraw,
-		int length
+		SGWorldObject* toDraw, // Pointer to first element of array
+		int length  // Length of array
 	);
 
 private:
-	GWindow* window;
-	SGCamera* camera;
+	GWindow* window;   // Window object
+	SGCamera* camera;  // Camera object
 };
 
 #endif
